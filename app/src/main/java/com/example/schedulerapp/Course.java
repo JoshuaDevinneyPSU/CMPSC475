90a5f7401f02c84cb0ff19d4436b63fbdb10aeaa
Joshua Devinney <jzd278@psu.edu>
2021-11-07 14:38:31 -0500

    Created Course Model with Constructor, Setters, and Getters.

---
+++ app/src/main/java/com/example/schedulerapp/Course.java
@@ -0,0 +1,67 @@
+package com.example.schedulerapp;
+
+public class Course {
+    private int mId;
+    private String mName;
+    private String mProf;
+    private int mStart;
+    private int mEnd;
+    private String mLocation;
+
+    public Course(int mId, String mName, String mProf, int mStart, int mEnd, String mLocation) {
+        this.mId = mId;
+        this.mName = mName;
+        this.mProf = mProf;
+        this.mStart = mStart;
+        this.mEnd = mEnd;
+        this.mLocation = mLocation;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmProf() {
+        return mProf;
+    }
+
+    public void setmProf(String mProf) {
+        this.mProf = mProf;
+    }
+
+    public int getmStart() {
+        return mStart;
+    }
+
+    public void setmStart(int mStart) {
+        this.mStart = mStart;
+    }
+
+    public int getmEnd() {
+        return mEnd;
+    }
+
+    public void setmEnd(int mEnd) {
+        this.mEnd = mEnd;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+}
