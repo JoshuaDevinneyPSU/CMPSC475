fc7d787ca655b92bf69ae44da2419e1926cef611
Joshua Devinney <jzd278@psu.edu>
2021-11-07 15:29:08 -0500

    Started the CourseDb constructor and implemented the addWeekday function.

--- app/src/main/java/com/example/schedulerapp/CourseDatabase.java
+++ app/src/main/java/com/example/schedulerapp/CourseDatabase.java
@@ -1,5 +1,6 @@
 package com.example.schedulerapp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,5 +17,29 @@ public class CourseDatabase {
             mCourseDb = new CourseDatabase();
         }
         return mCourseDb;
+    }
+
+    private CourseDatabase() {
+        mWeek = new ArrayList<>();
+        mCourses = new HashMap<>();
+
+        Weekday weekday = new Weekday(1, "Monday");
+        addWeekday(weekday);
+        weekday = new Weekday(2, "Tuesday");
+        addWeekday(weekday);
+        weekday = new Weekday(3, "Wednesday");
+        addWeekday(weekday);
+        weekday = new Weekday(4, "Thursday");
+        addWeekday(weekday);
+        weekday = new Weekday(5, "Friday");
+        addWeekday(weekday);
+
+
+    }
+
+    private void addWeekday(Weekday weekday) {
+        mWeek.add(weekday);
+        List<Course> courseList = new ArrayList<>();
+        mCourses.put(weekday.getmId(), courseList);
     }
 }
