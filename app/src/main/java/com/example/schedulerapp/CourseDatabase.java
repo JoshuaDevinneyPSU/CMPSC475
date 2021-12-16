817498ad953ddfafced7691300eff11a039f101d
JoshuaDevinney <jzd278@psu.edu>
2021-12-16 18:53:34 -0500

    Filled in Database Added in temporary courses to each day for use in the Demo.

--- app/src/main/java/com/example/schedulerapp/CourseDatabase.java
+++ app/src/main/java/com/example/schedulerapp/CourseDatabase.java
@@ -32,9 +32,39 @@ public class CourseDatabase {
         weekday = new Weekday(5, "Friday");
         addWeekday(weekday);
 
-        Course course = new Course(1, 1, "CMPSC 475", "Dr. Xiao", "3:00pm", "4:00pm", "Burke 180");
+        Course course = new Course(1, 1, "CMPSC 475", "Dr. Xiao", "3:00pm", "4:00pm", "Burke 105");
         addCourse(course);
+        course = new Course(2, 1, "CMPSC 461", "Dr. Hoblos", "11:00am", "12:15pm", "Burke 107");
+        addCourse(course);
+        course = new Course(3, 1, "ENGL 202C", "Dr. Gallagher", "8:00am", "9:00am", "Kochel 025");
+        addCourse(course);
+
+        course = new Course(1, 3, "CMPSC 475", "Dr. Xiao", "3:00pm", "4:00pm", "Burke 105");
+        addCourse(course);
+        course = new Course(2, 3, "CMPSC 461", "Dr. Hoblos", "11:00am", "12:15pm", "Burke 107");
+        addCourse(course);
+        course = new Course(3, 3, "ENGL 202C", "Dr. Gallagher", "8:00am", "9:00am", "Kochel 025");
+        addCourse(course);
+        course = new Course(4, 3, "SWENG 465 Lab", "Dr. Sammoud", "1:00pm", "2:30pm", "AMIC 110");
+        addCourse(course);
+
+        course = new Course(1, 5, "CMPSC 475", "Dr. Xiao", "3:00pm", "4:00pm", "Burke 105");
+        addCourse(course);
+        course = new Course(2, 5, "CMPSC 461", "Dr. Hoblos", "11:00am", "12:15pm", "Burke 107");
+        addCourse(course);
+        course = new Course(3, 5, "ENGL 202C", "Dr. Gallagher", "8:00am", "9:00am", "Kochel 025");
+        addCourse(course);
+        course = new Course(4, 5, "SWENG 465 Lab", "Dr. Sammoud", "1:00pm", "2:30pm", "AMIC 110");
+        addCourse(course);
+
         course = new Course(1, 2, "GEOG 101", "Dr. Naber", "5:00pm", "6:00pm", "Nick 130");
+        addCourse(course);
+        course = new Course(1, 2, "GYM 101", "Coach Jim", "10:00am", "12:00pm", "Junker 05");
+        addCourse(course);
+
+        course = new Course(1, 4, "GEOG 101", "Dr. Naber", "5:00pm", "6:00pm", "Nick 130");
+        addCourse(course);
+        course = new Course(1, 4, "GYM 101", "Coach Jim", "10:00am", "12:00pm", "Junker 05");
         addCourse(course);
 
     }
